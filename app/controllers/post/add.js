2df1777917bcd61bdb267ddcf5502422d46678d8
Caleb Cauthon <calebcauthon@gmail.com>
2015-11-19 13:50:25 +1300

    Redirect to sorted blog posts after adding a new blog post

--- app/controllers/post/add.js
+++ app/controllers/post/add.js
@@ -3,12 +3,16 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     addPost() {
+      var self = this;
+
       var post = this.store.createRecord('post', { 
         title: this.get('title'),
         body: this.get('body')
       });
-      
-      post.save();
+
+      post.save().then(function() {
+        self.transitionToRoute('blog')
+      });
 
       return false;
     }
