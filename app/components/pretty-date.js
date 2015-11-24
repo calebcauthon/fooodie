0072007b5a456a8e7ba9132c7e37b2be095c50c2
Caleb Cauthon <calebcauthon@gmail.com>
2015-11-24 22:19:27 +1300

    Clean up some styling.

--- app/components/pretty-date.js
+++ app/components/pretty-date.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  tagName: '',
   positionalParams: ['uglyDate'],
   prettyDate: function() {
     return moment(this.get('uglyDate')).format('ll')
