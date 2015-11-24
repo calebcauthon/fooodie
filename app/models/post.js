163ad91e83ef345a988804a31400d963f35c8757
Caleb Cauthon <calebcauthon@gmail.com>
2015-11-24 22:12:29 +1300

    Show real posts within the theme.

--- app/models/post.js
+++ app/models/post.js
@@ -4,5 +4,8 @@ export default DS.Model.extend({
   title: DS.attr(),
   created_at: DS.attr(),
   body: DS.attr(),
-  user: DS.belongsTo('user')
+  user: DS.belongsTo('user'),
+  subtitle: function() {
+    return this.get('body').substr(0, 15) + '...';
+  }.property('title')
 });
