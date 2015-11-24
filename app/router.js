3e76dafca35c3dd4ce02446e9469c686bcb97f6c
Caleb Cauthon <calebcauthon@gmail.com>
2015-11-24 22:45:01 +1300

    Style the view-post page.

--- app/router.js
+++ app/router.js
@@ -9,6 +9,7 @@ Router.map(function() {
   this.route('blog', { path: '/blog' });
   this.route('favorites', { path: '/favs' });
   this.resource('post', function() {
+    this.route('view', { path: '/view/:post_id' });
     this.route('add');
     this.route('remove', { path: '/remove/:post_id' });
     this.route('edit', { path: '/edit/:post_id' });
