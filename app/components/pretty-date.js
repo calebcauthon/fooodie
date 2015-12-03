70be92701a464f2d4bb2b78c7bbfa81c5fc51967
Caleb Cauthon <calebcauthon@gmail.com>
2015-12-03 23:00:43 +1300

    Use reopenClass to comply with deprecation warnings.

--- app/components/pretty-date.js
+++ app/components/pretty-date.js
@@ -2,8 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: '',
-  positionalParams: ['uglyDate'],
   prettyDate: function() {
     return moment(this.get('uglyDate')).format('ll')
   }.property('uglyDate')
+}).reopenClass({
+  positionalParams: ['uglyDate']
 });
